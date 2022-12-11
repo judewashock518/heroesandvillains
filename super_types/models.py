7211da9a9e2db4f18f357312f2bd327988c5fcc4
JW <114599894+judewashock518@users.noreply.github.com>
2022-12-11 18:19:44 -0500

    added supers app, created super class, added models and foreignkey

--- super_types/models.py
+++ super_types/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from supers.models import Super
 
 # Create your models here.
 
 class SuperType(models.Model):
     type = models.CharField(max_length=255)
-    
+    super_type = models.ForeignKey(Super, on_delete=models.CASCADE)
+
 

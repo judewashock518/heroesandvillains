021284144f1996764f1edf38ac1e18de7d769cb8
JW <114599894+judewashock518@users.noreply.github.com>
2022-12-11 17:35:29 -0500

    created request serializer data in views file and incorporated postman

--- super_types/views.py
+++ super_types/views.py
@@ -1,7 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SuperTypeSerializer
+from .models import SuperType
+
 
 @api_view(['GET'])
 def type_list(request):
+    type = SuperType.objects.all()
+    serializer = SuperTypeSerializer(type, many=True)
 
-    return Response('ok')
+
+    return Response(serializer.data)

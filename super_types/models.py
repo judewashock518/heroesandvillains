373d6839ea9e42ce771b6313eb646a5f2c880087
JW <114599894+judewashock518@users.noreply.github.com>
2022-12-11 22:25:41 -0500

    created supers app, serializers, urls, and models pages and starting to incorporate postman

--- super_types/models.py
+++ super_types/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-from supers.models import Super
 
 # Create your models here.
 
 class SuperType(models.Model):
     type = models.CharField(max_length=255)
-    super_type = models.ForeignKey(Super, on_delete=models.CASCADE)
-
 

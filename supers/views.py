4a5ed0fe57fda5bfa58a331413a2cacd9752cb18
JW <114599894+judewashock518@users.noreply.github.com>
2022-12-12 00:11:38 -0500

    created functionality to update a specific super in the supers app views file

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,13 +20,16 @@ def supers_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def supers_detail(request, pk):
-    try:
-        supers = Super.objects.get(pk=pk)
+    supers = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
         serializer = SuperSerializer(supers)
         return Response(serializer.data)
-    except Super.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND);
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(supers, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
